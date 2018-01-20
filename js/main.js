affcc1edbeb877454aec60642c6af106255ad62d
Adam Todryk <adam.todryk@gmail.com>
2018-01-20 23:20:13 +0100

    /project fed b4 full check

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var here = document.querySelector('.tabholder');
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var mailers = ['@gmail.com','@wp.pl','@onet.pl','@hotmail.com'];
+function randomizer(){
+var firstname = ""
+var lastname = ""
+var tel = ""
+var mail =""
+var arrayy = []
+for (var k= 0; k<Math.floor(Math.random()*10 +6); k++){
+var sign = alphabet[Math.floor(Math.random()*alphabet.length)]; 
+firstname+= sign;
+}
+for (var l= 0; l<Math.floor(Math.random()*10 +6); l++){
+var sign2 = alphabet[Math.floor(Math.random()*alphabet.length)];
+lastname+=sign2;
+}
+for (var m =0; m<9; m++){
+var num = Math.floor(Math.random()*10);
+tel+= num;
+}
+for (var n = 0; n<Math.floor(Math.random()*10+4);n++) {
+var sign3 = alphabet[Math.floor(Math.random()*alphabet.length)];
+mail+= sign3;
+}
+mail +=mailers[Math.floor(Math.random()*3)];
+
+      var tab= document.querySelector('.tab1');
+    var ar = [];
+    ar[0] = firstname;
+    ar[1] = lastname;
+    ar[2] = tel;
+    ar[3] = mail;
+    var hmi = document.getElementsByTagName('td');
+    var check = 0
+    for (var j= 0; j<hmi.length; j++){
+    if (hmi[j].textContent === ar[2] || hmi[j].textContent === ar[3]){
+        check +=1
+    }
+    else{
+    check=check
+}}
+    if (check ===0){
+    var MyRow = document.createElement('tr');
+    for (var i= 0; i<4; i++){
+    var MyCell= document.createElement('td');
+    MyCell.textContent = ar[i];
+    MyRow.appendChild(MyCell);
+    }
+    tab.appendChild(MyRow);
+    here.appendChild(tab);
+}
+else {
+  alert('This telephone or email already exists - please select another');
+}
+}
+function foo(){
+    var tab= document.querySelector('.tab1');
+    var ar = [];
+    ar[0] = document.getElementById('firstname').value;
+    ar[1] = document.getElementById('lastname').value;
+    ar[2] = document.getElementById('tel').value;
+    ar[3] = document.getElementById('mail').value;
+    var hmi = document.getElementsByTagName('td');
+    var check = 0
+    for (var j= 0; j<hmi.length; j++){
+    if (hmi[j].textContent === ar[2] || hmi[j].textContent === ar[3]){
+        check +=1
+    }
+    else{
+    check=check
+}}
+    if (check ===0){
+    var MyRow = document.createElement('tr');
+    for (var i= 0; i<4; i++){
+    var MyCell= document.createElement('td');
+    MyCell.textContent = ar[i];
+    MyRow.appendChild(MyCell);
+    }
+    tab.appendChild(MyRow);
+    here.appendChild(tab);
+}
+else {
+  alert('This telephone or email already exists - please select another');
+}}
+btn1.addEventListener('click', function(){
+    foo();
+})
+btn2.addEventListener('click',function(){
+    randomizer();
+})
